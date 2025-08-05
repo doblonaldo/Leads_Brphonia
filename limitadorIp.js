56dd8addaeacb58555cfbc7b1a9616b553e9b5ad
Laiber22 <lucassouza1@unigranrio.br>
2025-08-05 15:47:29 -0300

    Final

--- limitadorIp.js
+++ limitadorIp.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limitadorIp = rateLimit({
   windowMs: 1 * 60 * 1000, // minutos
-  max: 1, // Máximo de 10 requisições por IP
+  max: 5, // Máximo de 10 requisições por IP
   message: {
     error: 'Muitas requisições vindas deste IP. Tente novamente mais tarde.'
   },
