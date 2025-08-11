a9080a93e70a032daa581a3b5e585b44c7091bb6
Laiber22 <lucassouza1@unigranrio.br>
2025-08-11 11:49:52 -0300

    api

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const helmet = require('helmet');
 let isDebugMode = false;
 
 // --- 2. MIDDLEWARES ---
-app.set('trust proxy', false);
+app.set('trust proxy', 1);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
